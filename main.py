baeab0737982e8e820a43076fe75b9fe7545eb83
David Winterbottom <david.winterbottom@gmail.com>
2022-11-25 22:10:44 +0000

    Ensure the same image isn't printed twice
    By using an archive JSON file to keep track.

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ Usage:
 
     $ python main.py
 """
-from typing import Iterator, Tuple
+import json
+import os
+import random
+from typing import Iterator, List, Tuple, Set
 
 import twitter  # type: ignore
 
@@ -18,6 +21,7 @@ def random_image_markdown() -> str:
         "HenryRothwell",
         "womensart1",
         "ahistoryinart",
+        "CanadaPaintings",
     )
     client = _twitter_client()
 
@@ -42,20 +46,41 @@ def _fetch_random_image(
     client: twitter.Api, usernames: Tuple[str, ...]
 ) -> Tuple[str, str]:
     """
-    Return the image URL and tweet URL for a random image.
+    Return the image URL and tweet URL for a random image from the passed accounts.
     """
-    # TODO track which images have been printed out before.
-    for username in usernames:
-        statuses = client.GetUserTimeline(screen_name=username, count=200)
-        for status in statuses:
-            image_urls = set(_image_urls(status))
-            if image_urls:
+    # Shuffle the Twitter accounts we look at.
+    shuffled_usernames = list(usernames)
+    random.shuffle(shuffled_usernames)
+
+    # Loop through the first 200 tweets to find a picture we haven't used before.
+    for username in shuffled_usernames:
+        for status, image_urls in _image_tweets(client, username):
+            if not _has_tweet_been_used_before(status):
+                _record_tweet_use(status)
                 return (
                     image_urls.pop(),
                     f"https://twitter.com/{username}/status/{status.id}",
                 )
 
     raise RuntimeError("No image tweets found")
+
+
+def _image_tweets(client: twitter.Api, username: str) -> Iterator[Tuple[twitter.Status, Set[str]]]:
+    """
+    Return an iterator of tweets that have linked images.
+    """
+    max_id = None
+    while True:
+        statuses = client.GetUserTimeline(screen_name=username, max_id=max_id, count=200)
+        # Break the loop once we run out of pages.
+        if len(statuses) <= 1:
+            return
+        for status in statuses:
+            # Fetch images linked to tweet.
+            image_urls = set(_image_urls(status))
+            if image_urls:
+                yield status, image_urls
+            max_id = status.id
 
 
 def _image_urls(status: twitter.Status) -> Iterator[str]:
@@ -79,6 +104,35 @@ def _image_urls(status: twitter.Status) -> Iterator[str]:
     else:
         for entity in entities:
             yield entity["media_url"]
+
+
+# Archive utils
+
+
+ARCHIVE_FILEPATH = "archive.json"
+
+
+def _has_tweet_been_used_before(status: twitter.Status) -> bool:
+    used_ids = _load_used_tweet_ids()
+    return status.id in used_ids
+
+
+def _record_tweet_use(status: twitter.Status) -> None:
+    used_ids = _load_used_tweet_ids()
+    used_ids.append(status.id)
+    _save_used_tweet_ids(used_ids)
+
+
+def _load_used_tweet_ids() -> List[str]:
+    if os.path.exists(ARCHIVE_FILEPATH):
+        with open(ARCHIVE_FILEPATH, "r") as f:
+            return json.load(f)
+    return []
+
+
+def _save_used_tweet_ids(tweet_ids: List[str]) -> None:
+    with open(ARCHIVE_FILEPATH, "w") as f:
+        return json.dump(tweet_ids, f)
 
 
 if __name__ == "__main__":
